1a3cf18e6933fae4166a0bc6b0af474eaa0ae0c2
huirad <huirad@github.com>
2019-04-06 14:57:17 +0200

    aes cbc gcm

---
+++ cryptopoc/gcm_test.c
@@ -0,0 +1,209 @@
+/* OpenSSL Test program for AES-GCM encryption
+ * 
+ * @ref https://wiki.openssl.org/index.php/Libcrypto_API
+ * 
+ * https://wiki.openssl.org/index.php/EVP
+ * 
+ * https://wiki.openssl.org/index.php/EVP_Symmetric_Encryption_and_Decryption
+ * 
+ * https://wiki.openssl.org/index.php/EVP_Authenticated_Encryption_and_Decryption
+ * 
+ * https://www.openssl.org/docs/man1.1.0/man3/EVP_CIPHER_CTX_new.html
+ */
+
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <string.h>
+
+
+
+void handleErrors(void)
+{
+  ERR_print_errors_fp(stderr);
+  abort();
+}
+
+
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *aad,
+	int aad_len, unsigned char *key, unsigned char *iv, int iv_len,
+	unsigned char *ciphertext, unsigned char *tag)
+{
+	EVP_CIPHER_CTX *ctx;
+
+	int len;
+
+	int ciphertext_len;
+
+
+	/* Create and initialise the context */
+	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+	/* Initialise the encryption operation. */
+	if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_gcm(), NULL, NULL, NULL))
+		handleErrors();
+
+	/* Set IV length if default 12 bytes (96 bits) is not appropriate */
+	if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv_len, NULL))
+		handleErrors();
+
+	/* Initialise key and IV */
+	if(1 != EVP_EncryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
+
+	/* Provide any AAD data. This can be called zero or more times as
+	 * required
+	 */
+    if (aad)
+	if(1 != EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len))
+		handleErrors();
+
+	/* Provide the message to be encrypted, and obtain the encrypted output.
+	 * EVP_EncryptUpdate can be called multiple times if necessary
+	 */
+	if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+		handleErrors();
+	ciphertext_len = len;
+
+	/* Finalise the encryption. Normally ciphertext bytes may be written at
+	 * this stage, but this does not occur in GCM mode
+	 */
+	if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+	ciphertext_len += len;
+
+	/* Get the tag */
+	if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, tag))
+		handleErrors();
+
+	/* Clean up */
+	EVP_CIPHER_CTX_free(ctx);
+
+	return ciphertext_len;
+}
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *aad,
+	int aad_len, unsigned char *tag, unsigned char *key, unsigned char *iv,
+	int iv_len, unsigned char *plaintext)
+{
+	EVP_CIPHER_CTX *ctx;
+	int len;
+	int plaintext_len;
+	int ret;
+
+	/* Create and initialise the context */
+	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+	/* Initialise the decryption operation. */
+	if(!EVP_DecryptInit_ex(ctx, EVP_aes_128_gcm(), NULL, NULL, NULL))
+		handleErrors();
+
+	/* Set IV length. Not necessary if this is 12 bytes (96 bits) */
+	if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv_len, NULL))
+		handleErrors();
+
+	/* Initialise key and IV */
+	if(!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
+
+	/* Provide any AAD data. This can be called zero or more times as
+	 * required
+	 */
+    if (aad)
+	if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len))
+		handleErrors();
+
+	/* Provide the message to be decrypted, and obtain the plaintext output.
+	 * EVP_DecryptUpdate can be called multiple times if necessary
+	 */
+	if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+		handleErrors();
+	plaintext_len = len;
+
+	/* Set expected tag value. Works in OpenSSL 1.0.1d and later */
+	if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag))
+		handleErrors();
+
+	/* Finalise the decryption. A positive return value indicates success,
+	 * anything else is a failure - the plaintext is not trustworthy.
+	 */
+	ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+
+	/* Clean up */
+	EVP_CIPHER_CTX_free(ctx);
+
+	if(ret > 0)
+	{
+		/* Success */
+		plaintext_len += len;
+		return plaintext_len;
+	}
+	else
+	{
+		/* Verify failed */
+		return -1;
+	}
+}
+
+int main (void)
+{
+  /* Set up the key and iv. Do I need to say to not hard code these in a
+   * real application? :-)
+   */
+
+  /* A 256 bit key */
+  unsigned char *key = (unsigned char *)"0123456789012345";
+
+  /* A 128 bit IV */
+  unsigned char *iv = (unsigned char *)"012345678901";
+
+  /* Message to be encrypted */
+  unsigned char *plaintext =
+                (unsigned char *)"Halloele";
+
+  /* Buffer for ciphertext. Ensure the buffer is long enough for the
+   * ciphertext which may be longer than the plaintext, dependant on the
+   * algorithm and mode
+   */
+  unsigned char ciphertext[128];
+
+  /* Buffer for the decrypted text */
+  unsigned char decryptedtext[128];
+
+  /* Buffer for the tag */
+  unsigned char tag[128];
+
+
+  int decryptedtext_len, ciphertext_len;
+
+
+  /* Encrypt the plaintext */
+  ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext),
+                            NULL, 0,
+                            key,
+                            iv, 12,
+                            ciphertext,
+                            tag
+                           );
+
+  /* Do something useful with the ciphertext here */
+  printf("Ciphertext is:\n");
+  BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+  printf("TAG is:\n");
+  BIO_dump_fp (stdout, (const char *)tag, 16);  
+
+  /* Decrypt the ciphertext */
+  decryptedtext_len = decrypt(ciphertext, ciphertext_len,
+                              NULL, 0,
+                              tag,
+                              key,
+                              iv, 12,
+                              decryptedtext);
+
+  /* Add a NULL terminator. We are expecting printable text */
+  decryptedtext[decryptedtext_len] = '\0';
+
+  /* Show the decrypted text */
+  printf("Decrypted text is:\n");
+  printf("%s\n", decryptedtext);
+
+
+  return 0;
+}
